4ef9917e169e516f1d9af3d7109ddc56eee11bae
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2019-07-10 18:39:21 +0200

    Adding scss support

--- src/lib/parseStyleSheet.ts
+++ src/lib/parseStyleSheet.ts
@@ -1,6 +1,22 @@
 import { readFileSync } from 'fs';
+import * as sass from 'node-sass';
 
 export const parseStyleSheet = (stylesheet: string): string => {
+
+  if (!stylesheet) return '';
+
+  if (stylesheet.includes('.scss')) {
+    try {
+      const styles = sass.renderSync({
+        file: stylesheet
+      });
+
+      return styles.css;
+    } catch (e) {
+      return '';
+    }
+  }
+
   try {
     const styles = readFileSync(stylesheet, 'utf8');
 
@@ -8,4 +24,5 @@ export const parseStyleSheet = (stylesheet: string): string => {
   } catch (e) {
     return '';
   }
+
 };
